e12eeac32fdfbb7d2193862508c11a293c9cda23
Jayditya Singh <89415749+sjayditya@users.noreply.github.com>
2022-01-01 23:18:44 +0530

    Add files via upload

---
+++ Task2/led.cpp/led.cpp.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(3000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
